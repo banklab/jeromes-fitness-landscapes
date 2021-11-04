8736ddec09f3dc44b85c7cdc795e76fe6caf8a2d
JeromeStaeheli <92435129+JeromeStaeheli@users.noreply.github.com>
2021-11-04 15:16:42 +0100

    Add files via upload

---
+++ Fisher ver 1.02.py
@@ -0,0 +1,73 @@
+# Load Packages
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+# Allele Frequencies
+allele_freq_1 = 0.5
+# Population size
+pop_start = 100
+# Number of generations
+gen_time = 500
+
+# Calculate the faculty
+def fac(fac_num):
+    fac = 1
+    for num_fac in range(1, fac_num + 1):
+        fac = fac * num_fac
+    return fac
+
+
+# Calculate probability with the binominal coefficient
+def prob_pop(pop_size, all_freq_1):
+    
+    # Create empty list for noting every chance to pass on certain amount of copies of one allel
+    prob_list = []
+    
+    # Fisher calculation for the chance to pass on copies of a allele
+    for num_prob in range(0, (2 * pop_size) + 1):
+        prob = (((fac(2 * pop_size))
+                / (fac(num_prob) * fac(2 * pop_size-num_prob)))
+                * (all_freq_1 ** num_prob)
+                * ((1 - all_freq_1) ** (2 * pop_size-num_prob)))
+        prob_list.append(prob)
+
+    return prob_list
+
+
+# Calculate allele frequencies of next generation
+def next_gen(pop_size, all_freq_1):
+
+    # Choose a amount of alleles from one type of alleles with the calculated probability of prob_pob function
+    next_gen = np.random.choice(list(range(0, (2 * pop_start) + 1)), 
+                                1, 
+                                p = prob_pop(pop_start, all_freq_1))
+    all_freq_new = next_gen / (pop_size * 2)
+    
+    return all_freq_new[0]
+
+
+
+# Repeat the calculation of the new allele frequencies a chosen number of times
+
+def wright_fisher_model(gen_num, pop_size, all_freq_1):
+    
+    # Repeat the next_gen function to generate new allele frequencies from the previous allele frequencies
+    all_freq = [all_freq_1]
+    
+    for rep_calc in range(1, gen_num):
+        
+        # Add new allele frequency to frequency list
+        all_freq.append(next_gen(pop_size, all_freq[rep_calc-1]))
+        
+        # Terminate function when reaching 1 or 0 since it does not change anymore (to save time)
+        if all_freq[rep_calc-1] == 0 or all_freq[rep_calc-1] == 1:
+            break
+        
+    print(all_freq)
+    return all_freq
+
+
+allele_frequencies = wright_fisher_model(gen_time, pop_start, allele_freq_1)
+plt.plot(range(0, len(allele_frequencies)), allele_frequencies)
+
